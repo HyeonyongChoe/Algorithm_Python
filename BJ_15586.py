fade49325d9083925c6e04fa3a2f90e1cd1ecf52
HyeonyongChoe <gusdyddl1212@gmail.com>
2024-01-03 20:51:29 +0900

    24.01.03

--- BJ_15586.py
+++ BJ_15586.py
@@ -6,7 +6,7 @@ for i in range(N-1):
     p,q,r = map(int,input().split())
     USADO[p][q] = r
 
-for i in range(Q):
-    k, v = map(int,input().split())
-    for j in range(1,N+1):
-        
+#for i in range(Q):
+#    k, v = map(int,input().split())
+#    for j in range(1,N+1):
+# 값
